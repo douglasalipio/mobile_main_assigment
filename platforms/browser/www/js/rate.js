ca684078fe67a8907247f26d290560a6c171fa05
Douglas Mesquita <douglasalipio@gmail.com>
2019-04-14 15:42:55 +0100

    Div for labels

--- platforms/browser/www/js/rate.js
+++ platforms/browser/www/js/rate.js
@@ -1,11 +1,11 @@
 var defaultMoney = "USD"
-var layerAPIKey =  "ee5368d2967dffe44b5a"
+var layerAPIKey = "ee5368d2967dffe44b5a"
 
-function getRate(moneyIso){
+function getRate(moneyIso) {
     // The XMLHttpRequest object, is the one in 
     // charge of handleing the request for us
     var http = new XMLHttpRequest();
-    const url= 'https://free.currencyconverterapi.com/api/v6/convert?q='+defaultMoney+'_'+moneyIso+'&compact=ultra&apiKey='+layerAPIKey+'';
+    const url = 'https://free.currencyconverterapi.com/api/v6/convert?q=' + defaultMoney + '_' + moneyIso + '&compact=ultra&apiKey=' + layerAPIKey + '';
     // a "GET" request to the URL define above
     http.open("GET", url);
     // Sending the request
@@ -14,21 +14,15 @@ function getRate(moneyIso){
     // Once the request has been processed and we have
     // and answer, we can do something with it
     http.onreadystatechange = (e) => {
-        
+
         // First, I'm extracting the reponse from the 
         // http object in text format
         var response = http.responseText;
 
         // As we know that answer is a JSON object,
         // we can parse it and handle it as such
-        var responseJSON = JSON.parse(response); 
-        rate = responseJSON.USD_EUR;
-        console.log("opa"+rate);
-         // Printing the result JSON to the console
-        console.log(rate);
-        var oc3 = "Your Currency money is: "+moneyIso;
-        // Placing formatted data on the front ed
-        //document.getElementById('getWeather').innerHTML=oc3;
-        
+        var responseJSON = JSON.parse(response);
+        rate = responseJSON[`USD_${moneyIso}`];
+        document.getElementById('dynamicMoneyLabel').innerHTML = moneyIso
     }
 }
