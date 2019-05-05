9fb2512eb154c64eda89a2931766a9eacd9002d8
Douglas Mesquita <douglasalipio@gmail.com>
2019-05-05 22:17:22 +0100

    last commit

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,7 +1,7 @@
 
 // Initialize app
 var myApp = new Framework7({
-    
+
 });
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
@@ -11,25 +11,46 @@ var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
-
+var storage = window.localStorage;
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var fileEntryGlobal;
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function () {
     console.log("Device is ready!");
     getLocation();
     createDatabase();
-    loadStoreLocation();   
+    loadStoreLocation();
     getCurrentTime();
+    tryingFile();
 });
 
-function gotFS(fileSystem) {
-    fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
+function tryingFile() {
+    console.log("bora ver");
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemCallback, onError);
+
 }
-function fail(evt) {
-    console.log(evt.target.error.code);
+
+function fileSystemCallback(fs) {
+
+    // Name of the file I want to create
+    var fileToCreate = "newPersistentFile.txt";
+
+    // Opening/creating the file
+    fs.root.getFile(fileToCreate, fileSystemOptionals, getFileCallback, onError);
 }
+
+var fileSystemOptionals = { create: true, exclusive: false };
+
+var fileEntryGlobal;
+var contentGlobal = "";
+
+function getFileCallback(fileEntry) {
+    fileEntryGlobal = fileEntry;
+}
+
+
 
 function getLocation() {
     // Once the position has been retrieved, an JSON object
@@ -39,35 +60,44 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition(geoCallback, onError);
 }
 
-function getCurrentTime(){
-    console.log("horario"+ time);
+function getCurrentTime() {
+    console.log("horario" + time);
     document.getElementById('getCurrentTime').innerHTML = "time";
 }
 
 function initMap(lat, lng) {
 
     // Defining a position to display
-    var cct = {lat:lat, lng: lng};
-    
+    var cct = { lat: lat, lng: lng };
+
     // Creating the map, centred on the position 
     // defined above
     var myMap = new google.maps.Map(document.getElementById('map'),
-        {zoom: 18,
-        center: cct });
-    
+        {
+            zoom: 18,
+            center: cct
+        });
+
     // Creating a marker to place on the map
     // at the position defined above
     var marker = new google.maps.Marker(
-        { position: cct,
-        map: myMap });
-    
+        {
+            position: cct,
+            map: myMap
+        });
+
     // Adding another pointer
-    var otherLocation = {lat: lat, lng: -lng};
+    var otherLocation = { lat: lat, lng: -lng };
     var marker2 = new google.maps.Marker(
-        { position: otherLocation,
-        map: myMap });
+        {
+            position: otherLocation,
+            map: myMap
+        });
 
     // REMEMBER: I added some style to the style file
     // to be able to display the map!!!
-         
-} 
+
+}
+function onError(msg) {
+    console.log(msg);
+}
